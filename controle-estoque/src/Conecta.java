664bcdb3f667a42ca4b68d53471a215ce40dcf6b
brunosilva <brunosilva@ASPLAN-SESI6560.fiern.local>
2016-07-14 13:58:15 -0300

    Commit inicial do projeto

---
+++ controle-estoque/src/Conecta.java
@@ -0,0 +1,18 @@
+import org.hibernate.Session;
+
+import com.astoria.sistema.estoque.HibernateUtil;
+
+public class Conecta {
+	public static void main(String[] args) {
+		
+		Session session = null;
+		
+		try {
+			session = HibernateUtil.getSession().openSession();
+			System.out.println("Conectou!");
+		} finally {
+			session.close();
+			System.out.println("Fechou conexão!");
+		}
+	}
+}
---
+++ controle-estoque/src/com/astoria/sistema/estoque/HibernateUtil.java
@@ -0,0 +1,24 @@
+package com.astoria.sistema.estoque;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+	private static final SessionFactory session = buildSessionFactory();
+
+	private static SessionFactory buildSessionFactory() {
+		try {
+			Configuration cfg = new Configuration();
+			cfg.configure("hibernate.cfg.xml");
+			return cfg.buildSessionFactory();
+		} catch(Throwable e ) {
+			System.out.println("Erro ao carregar o hibernate.cfg.xml:\n " + e);
+			throw new ExceptionInInitializerError();
+		}
+	}
+
+	public static SessionFactory getSession() {
+		return session;
+	}
+}
